536f3cde5ca43eaf427ca9015f866b1543ea7bf7
Kyengwhan Jee <flowant@gmail.com>
2019-01-22 10:45:15 -0800

    clean up

--- authserver/src/main/java/org/flowant/website/controller/UserRest.java
+++ authserver/src/main/java/org/flowant/website/controller/UserRest.java
@@ -8,13 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserRest {
 
+//    //TODO response minimal information to browser
+//    @RequestMapping("/user")
+//    public Map<String, String> user(Principal principal) {
+//        Map<String, String> map = new LinkedHashMap<>();
+//        map.put("name", principal.getName());
+//        return map;
+//    }
+
+    //TODO response minimal information to browser
     @RequestMapping("/user")
     public Principal user(Principal principal) {
-        return principal; //TODO response minimal information to browser
+        return principal;
     }
 
     @RequestMapping("/admin")
     public Principal admin(Principal principal) {
-        return principal; //TODO response minimal information to browser
+        return principal;
     }
 }
