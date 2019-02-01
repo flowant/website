453a53869ed6bf263b478b23f263494c8dcb20b1
Kyengwhan Jee <flowant@gmail.com>
2019-02-01 07:52:24 -0800

    add cors & content api endpoint

--- gateway/src/main/java/org/flowant/website/gateway/GatewaySecurityConfiguration.java
+++ gateway/src/main/java/org/flowant/website/gateway/GatewaySecurityConfiguration.java
@@ -1,4 +1,4 @@
-package org.flowant.gateway;
+package org.flowant.website.gateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
