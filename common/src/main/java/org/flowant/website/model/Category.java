391d06c750a7b1881dbd735a9f8d876045731fee
Kyengwhan Jee <flowant@gmail.com>
2019-03-11 17:19:15 -0700

    add like notification category

--- common/src/main/java/org/flowant/website/model/Category.java
+++ common/src/main/java/org/flowant/website/model/Category.java
@@ -2,10 +2,12 @@ package org.flowant.website.model;
 
 public enum Category {
 
-    NC,  // new Content
+    NC,  // new Content, notify to followers
 
     NRV, // new Review
 
-    NRP; // new Reply
+    NRP, // new Reply
+
+    L;   // like
 
 }
