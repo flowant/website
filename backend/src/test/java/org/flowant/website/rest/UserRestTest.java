989f4e657f7d8c675f2041c0167a2f4e03716de3
Kyengwhan Jee <flowant@gmail.com>
2019-03-12 22:07:24 -0700

    add a testcase for deleting user with files

--- backend/src/test/java/org/flowant/website/rest/UserRestTest.java
+++ backend/src/test/java/org/flowant/website/rest/UserRestTest.java
@@ -5,13 +5,16 @@ import java.util.UUID;
 import org.flowant.website.BackendApplication;
 import org.flowant.website.model.User;
 import org.flowant.website.repository.UserRepository;
+import org.flowant.website.storage.FileStorage;
 import org.flowant.website.util.test.UserMaker;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import junitparams.JUnitParamsRunner;
+import reactor.test.StepVerifier;
 
 @RunWith(JUnitParamsRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes=BackendApplication.class)
@@ -31,6 +34,24 @@ public class UserRestTest extends RestWithRepositoryTest<User, UUID, UserReposit
     @Test
     public void testCrud() {
         super.testCrud();
+    }
+
+    @Test
+    public void testDeleteWithFiles() {
+        User user = UserMaker.smallRandom();
+        FileRestTest.postFiles(3, webTestClient).consumeWith(body -> user.setFileRefs(body.getResponseBody()));
+
+        repo.save(user).block();
+        cleaner.registerToBeDeleted(user); // in case of fails
+
+        webTestClient.delete()
+                .uri(UserRest.USER + UserRest.PATH_SEG_ID, user.getIdentity())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody().consumeWith(r -> {
+                    user.getFileRefs().forEach(fileRef -> Assert.assertFalse(FileStorage.exist(fileRef.getIdentity())));
+                    StepVerifier.create(repo.findById(user.getIdentity())).expectNextCount(0).verifyComplete();
+                });
     }
 
 }
