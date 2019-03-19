b24a5fab31879d65c9ae5478571622c831232ba2
Kyengwhan Jee <flowant@gmail.com>
2019-03-19 01:39:00 -0700

    clean up

--- frontend/src/app/backend.service.ts
+++ frontend/src/app/backend.service.ts
@@ -66,8 +66,8 @@ export class BackendService {
       return of(this.relation)
     } else {
       return this.getUser().pipe(
-          concatMap(user => this.getModel<Relation>(Model.Relation, user.identity)),
-          tap(relation => this.relation = relation)
+        concatMap(user => this.getModel<Relation>(Model.Relation, user.identity)),
+        tap(relation => this.relation = relation)
       );
     }
   }
@@ -78,10 +78,10 @@ export class BackendService {
         + "/" + followerId + "/" + followeeId;
 
     return this.http.post(url, null, writeOptions).pipe(
-        map(r => JSON.parse(r.body, reviver)),
-        tap(relation => this.relation = relation),
-        tap(relation => this.logger.trace('posted Relation:', relation)),
-        catchError(this.handleError<any>('postRelation'))
+      map(r => JSON.parse(r.body, reviver)),
+      tap(relation => this.relation = relation),
+      tap(relation => this.logger.trace('posted Relation:', relation)),
+      catchError(this.handleError<any>('postRelation'))
     );
   }
 
@@ -106,9 +106,9 @@ export class BackendService {
     let queryParams: string = nextInfo ? nextInfo : `?tag=${tag}&page=${page}&size=${size}`;
 
     return this.http.get(Config.searchUrl + queryParams, baseOptions).pipe(
-        map(r => RespWithLink.of<Content>(JSON.parse(r.body, reviver), r.headers.get("link"))),
-        tap(r => this.logger.trace('fetched mapped:', r)),
-        catchError(this.handleError<RespWithLink<Content>>(`getSearch query:${queryParams}`)));
+      map(r => RespWithLink.of<Content>(JSON.parse(r.body, reviver), r.headers.get("link"))),
+      tap(r => this.logger.trace('fetched mapped:', r)),
+      catchError(this.handleError<RespWithLink<Content>>(`getSearch query:${queryParams}`)));
   }
 
   getModels<T>(model: Model, nextInfo: string, queryName: string, queryValue: string, page?: string, size?: string)
@@ -120,24 +120,24 @@ export class BackendService {
     let queryParams: string = nextInfo ? nextInfo : `?${queryName}=${queryValue}&page=${page}&size=${size}`;
 
     return this.http.get(Config.getUrl(model) + queryParams, baseOptions).pipe(
-        map(r => RespWithLink.of<T>(JSON.parse(r.body, reviver), r.headers.get("link"))),
-        tap(r => this.logger.trace('fetched mapped:', r)),
-        catchError(this.handleError<RespWithLink<T>>(`getModels query:${queryParams}`)));
+      map(r => RespWithLink.of<T>(JSON.parse(r.body, reviver), r.headers.get("link"))),
+      tap(r => this.logger.trace('fetched mapped:', r)),
+      catchError(this.handleError<RespWithLink<T>>(`getModels query:${queryParams}`)));
   }
 
   getModel<T>(model: Model, idToPath: IdToPath): Observable<T> {
     const url = Config.getUrl(model) + '/' + idToPath.toString();
     return this.http.get(url, baseOptions).pipe(
-        map(r => JSON.parse(r.body, reviver)),
-        tap(m => this.logger.trace('got model:', m)),
-        catchError(this.handleError<T>(`getModel model=${model} idToPath=${idToPath}`)));
+      map(r => JSON.parse(r.body, reviver)),
+      tap(m => this.logger.trace('got model:', m)),
+      catchError(this.handleError<T>(`getModel model=${model} idToPath=${idToPath}`)));
   }
 
   postModel<T>(model: Model, entity: T): Observable<T> {
     return this.http.post(Config.getUrl(model), entity, writeOptions).pipe(
-        map(r => JSON.parse(r.body, reviver)),
-        tap(m => this.logger.trace('posted model:', model, m)),
-        catchError(this.handleError<T>('postModel'))
+      map(r => JSON.parse(r.body, reviver)),
+      tap(m => this.logger.trace('posted model:', model, m)),
+      catchError(this.handleError<T>('postModel'))
     );
   }
 
@@ -147,8 +147,8 @@ export class BackendService {
 
     this.logger.trace("deleteModel url:", url);
     return this.http.delete(url, writeOptions).pipe(
-        tap(r => this.logger.trace(`deleted model idToPath:${idToPath}, resp:`, r)),
-        catchError(this.handleError<string>('deleteModel'))
+      tap(r => this.logger.trace(`deleted model idToPath:${idToPath}, resp:`, r)),
+      catchError(this.handleError<string>('deleteModel'))
     );
   }
 
@@ -160,9 +160,9 @@ export class BackendService {
     uploadData.append('attachment', file[0], file[0].name);
 
     return this.http.post(Config.fileUrl + '/' + identity, uploadData, baseOptions).pipe(
-        map(r => JSON.parse(r.body, reviver)),
-        tap(r => this.logger.trace('added files:', r)),
-        catchError(this.handleError<FileRefs>('addFile')));
+      map(r => JSON.parse(r.body, reviver)),
+      tap(r => this.logger.trace('added files:', r)),
+      catchError(this.handleError<FileRefs>('addFile')));
   }
 
   addFiles(files: FileList): Observable<FileRefs[]> {
@@ -173,15 +173,15 @@ export class BackendService {
     }
 
     return this.http.post(Config.fileUrl, uploadData, baseOptions).pipe(
-        map(r => JSON.parse(r.body, reviver)),
-        tap(r => this.logger.trace('added files:', r)),
-        catchError(this.handleError<FileRefs[]>('addFiles', [])));
+      map(r => JSON.parse(r.body, reviver)),
+      tap(r => this.logger.trace('added files:', r)),
+      catchError(this.handleError<FileRefs[]>('addFiles', [])));
   }
 
   deleteFiles(files: FileRefs[]): Observable<any> {
     return this.http.post(Config.fileDeletesUrl, files, writeOptions).pipe(
-        tap(r => this.logger.trace('deleted files:', r)),
-        catchError(this.handleError<FileRefs[]>('deleteFiles')));
+      tap(r => this.logger.trace('deleted files:', r)),
+      catchError(this.handleError<FileRefs[]>('deleteFiles')));
   }
 
   acculateRepute(model: Model, idCid: IdCid, reputation: Reputation): Observable<Reputation> {
