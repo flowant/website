11b05af04784bfa580e3cd8c6818391a6c86a673
Kyengwhan Jee <flowant@gmail.com>
2019-03-26 10:53:57 -0700

    basic request response testcase

--- frontend/src/app/_services/backend.service.spec.ts
+++ frontend/src/app/_services/backend.service.spec.ts
@@ -1,12 +1,11 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { LoggerModule, NgxLoggerLevel, NGXLogger } from 'ngx-logger';
 
 import { BackendService } from './backend.service';
-import { User, Phone, CruTime, Reply } from '../_models';
+import { User } from '../_models';
 import { Config } from '../config';
-
 
 describe('BackendService', () => {
 
@@ -40,7 +39,7 @@ describe('BackendService', () => {
     httpTestingController.verify();
   });
 
-  fdescribe('#User', () => {
+  describe('#User', () => {
 
     beforeEach(() => {
       service = TestBed.get(BackendService);
@@ -67,6 +66,21 @@ describe('BackendService', () => {
       req.flush(expectedUser);
     });
 
+    it('signUpUser should return 409 status', () => {
+      let existUser: User = User.guest();
+
+      service.signUpUser(existUser).toPromise().then(
+        user => fail('expected to fail'),
+        error => expect(error.status).toBe(409, 'expected 409: Conflict staus')
+      );
+
+      const req = httpTestingController.expectOne(Config.signupUrl);
+      expect(req.request.method).toEqual('POST');
+
+      // respond with a 404 and the error message in the body
+      req.flush('user alread exist', {status: 409, statusText: 'Conflict'});
+    });
+
     it('getModel<User> should return expected user', () => {
       let expectedUser: User = User.guest();
 
@@ -84,6 +98,21 @@ describe('BackendService', () => {
       expect(req.request.method).toEqual('GET');
 
       req.flush(expectedUser);
+    });
+
+    it('getModel<User> should return empty', () => {
+      let expectedUser: User = User.guest();
+
+      service.getModel<User>(User, expectedUser.identity).toPromise().then(
+        user => expect(user).toBe(undefined),
+        fail
+      );
+
+      const req = httpTestingController.expectOne(Config.userUrl + '/' + expectedUser.identity);
+      expect(req.request.method).toEqual('GET');
+
+      // respond with a 404 and the error message in the body
+      req.flush('unknown identity', {status: 404, statusText: 'Not Found'});
     });
 
   });
